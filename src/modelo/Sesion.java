2a91e898995fcb0eea41663f58e9f568f5875e36
sergio <sergiovergaraehu@gmail.com>
2020-03-25 18:33:16 +0100

    #11 Sesion almacena el nombre de jugador, la puntuacion, y la medida del tablero

--- src/modelo/Sesion.java
+++ src/modelo/Sesion.java
@@ -2,14 +2,35 @@ package modelo;
 
 public class Sesion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-
-
+    public String nombreJugador;
+    public String medidaTablero;
+    public int puntuacion;
+        
+    public Sesion(String nomJugador,String medTablero) {
+    	nombreJugador = nomJugador;
+    	medidaTablero = medTablero;
+    }
+    public Sesion(String nomJugador,String medTablero, int puntos) {
+    	nombreJugador = nomJugador;
+    	medidaTablero = medTablero;
+    	puntuacion = puntos;
+    }
+    
+    public String obtenerNombre() {
+    	return nombreJugador;
+    }
+    
+    public String obtenerMedidaTablero() {
+    	return medidaTablero;
+    }
+    
+    public String obtenerPuntuacion() {
+    	return medidaTablero;
+    }
+    
+    public int establecerPuntuacion(int puntos) {
+    	return puntuacion;
+    }
 
 }
+
